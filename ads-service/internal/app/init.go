7c9250fca5e4a55f5c9d55ab118d5727910a6ad5
Rabboni <rabboni171@gmail.com>
2024-08-17 10:16:29 +0500

    refactor:refactore Initilization func, add graceful shutdown

--- ads-service/internal/app/init.go
+++ ads-service/internal/app/init.go
@@ -8,6 +8,10 @@ import (
 	"ads-service/logger"
 	"context"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"net/http"
 
@@ -15,27 +19,53 @@ import (
 )
 
 func Initialization() {
-	// Иницилизация конфига
+	// Initialize config
 	config.MustLoad()
-	
-	// Иницилизация логгера
+
+	// Initialize logger
 	logger.MustLoad()
 
-	// Новый конкекст
+	// New context
 	ctx := context.Background()
 
-	// Иницилизация хранилища
+	// Initialize repository
 	repository := repository.NewRepository(ctx)
 
-	// Иницилизация контроллеров
+	// Initialize controllers
 	router := controllers.New(ctx, services.NewService(repository))
 
-	// Запуск сервера
+	// Create the HTTP server
 	serverAddr := config.Cfg.HTTPServerAddress
-	slog.Info("starting server at port: " + serverAddr)
-
-	if err := http.ListenAndServe(serverAddr, router); err != nil {
-		slog.Error("unable to start server", "err", err.Error())
-		return
+	srv := &http.Server{
+		Addr:    serverAddr,
+		Handler: router,
 	}
+
+	// Start the server in a goroutine
+	go func() {
+		slog.Info("starting server at port: " + serverAddr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("unable to start server", "err", err.Error())
+		}
+	}()
+
+	// Set up channel to listen for OS signals
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received
+	<-quit
+	slog.Info("Shutting down server...")
+
+	// Create a context with a timeout for the shutdown
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Attempt to gracefully shut down the server
+	if err := srv.Shutdown(ctxShutDown); err != nil {
+		slog.Error("Server forced to shutdown", "err", err.Error())
+	} else {
+		slog.Info("Server exited gracefully")
+	}
+
 }
